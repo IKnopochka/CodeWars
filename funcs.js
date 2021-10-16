39be3b0c64828a27f9efb9146b397ed5e2b90e83
Irina Novakovich <irinaredhood@gmail.com>
2021-10-16 16:26:56 +0300

    + function greet

--- funcs.js
+++ funcs.js
@@ -2,3 +2,8 @@ function isPalindrome(x) {
   // your code here
   return x.toLowerCase() == x.toLowerCase().split('').reverse().join("");
 }
+
+// Write a function "greet" that returns "hello world!"
+function greet() {
+  return "hello world!";
+};
