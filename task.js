127f8c3f4c64910e9b17932fa3f3d2e9628e77d3
Irina Novakovich <irinaredhood@gmail.com>
2022-08-31 16:18:03 +0300

    Write function RemoveExclamationMarks which removes all exclamation marks from a given string.

--- task.js
+++ task.js
@@ -1,43 +1,47 @@
 function arrAdder(arr) {
-  let newArray = []
-  for (let i = 0; i < arr[0].length; i++) {
-    for ( let j = 0; j < arr.length; j++) {
-      if (arr[j][i] !== '') {
-        newArray.push(arr[j][i])
-      } else {
-        break
-      }
+    let newArray = []
+    for (let i = 0; i < arr[0].length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j][i] !== '') {
+                newArray.push(arr[j][i])
+            } else {
+                break
+            }
+        }
+        newArray.push([' '])
     }
-    newArray.push([' '])
-  }
-  let result = newArray.join('').trim()
-  return result;
+    let result = newArray.join('').trim()
+    return result;
 }
 
 function rowSumOddNumbers(n) {
 
-  let startingNumber = 0; 
-  if (n > 2) {
-    startingNumber = n * (n - 1) + 1
-  } else if (n === 2) {
-    startingNumber = n + n - 1
-  } else {
-    startingNumber = 1
-  }
-  
-  let sum = startingNumber;
-  
-  if (sum === 1) {
-    return 1
-  } else {
-      for (let i = n; i > 1; i--) {
-        sum = sum + startingNumber + 2
-        startingNumber = startingNumber + 2
-      }
-    return sum
-  }
+    let startingNumber = 0;
+    if (n > 2) {
+        startingNumber = n * (n - 1) + 1
+    } else if (n === 2) {
+        startingNumber = n + n - 1
+    } else {
+        startingNumber = 1
+    }
+
+    let sum = startingNumber;
+
+    if (sum === 1) {
+        return 1
+    } else {
+        for (let i = n; i > 1; i--) {
+            sum = sum + startingNumber + 2
+            startingNumber = startingNumber + 2
+        }
+        return sum
+    }
 }
 
-function reverseWords(str){
-  return str.split(' ').reverse().join(" "); // reverse those words
+function reverseWords(str) {
+    return str.split(' ').reverse().join(" "); // reverse those words
+}
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, '');
 }
