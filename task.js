2554c5810c0ea24596854f330b2da634e0c829c6
Irina Novakovich <irinaredhood@gmail.com>
2022-10-12 14:16:11 +0300

    Done

--- task.js
+++ task.js
@@ -48,9 +48,9 @@ function removeExclamationMarks(s) {
 
 
 function feast(beast, dish) {
-  if(beast[0] === dish[0]){
-    if(beast[beast.length - 1] === dish[dish.length - 1]) return true
-    return false
-  }
+  return beast[0] == dish[0] 
+    ? beast[beast.length - 1] == dish[dish.length - 1] 
+  ? true : false
+    : false
 //your function here
 }
