0bcb00d11686d14c797d31b54add90f136fe45fb
Irina Novakovich <irinaredhood@gmail.com>
2022-11-09 18:21:12 +0300

    Strange principal

--- studentsFinalGrade.js
+++ studentsFinalGrade.js
@@ -1,1 +1,7 @@
 const finalGrade = (exam, projects) => exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+
+
+function numOfOpenLockers(n){
+
+    return Math.floor(Math.sqrt(n))
+}
