0a3c2cf269ef9fd2d1f627bcf2945d7eeb0abf69
Irina Novakovich <irinaredhood@gmail.com>
2023-01-11 14:19:20 +0300

    you will be given a positive integer, val and you have to create the function next_pal()(nextPal Javascript) that will output the smallest palindrome number higher than val.

--- realFloor.js
+++ realFloor.js
@@ -1,1 +1,7 @@
 const getRealFloor = n => n > 0 ? n < 13 ? n - 1 : n - 2 : n;
+
+function nextPal(val) {
+    return Number(String(val + 1).split('').reverse().join('')) === (val + 1)
+        ? (val + 1)
+        : nextPal(val + 1)
+}
