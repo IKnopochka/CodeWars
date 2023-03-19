bf4e3f96ce327d2147b2e598e556e77a23a5275f
Irina Novakovich <irinaredhood@gmail.com>
2023-03-19 16:16:01 +0300

    Your task is to remove all consecutive duplicate words from a string, leaving only first words entries.

--- areYouPlayingBanjo.js
+++ areYouPlayingBanjo.js
@@ -8,3 +8,16 @@ function fusc(n) {
     else if (!(n % 2)) return fusc(n / 2)
     else if (!!(n % 2)) return fusc((n - 1) / 2) + fusc((n - 1) / 2 + 1)
 }
+
+function removeConsecutiveDuplicates(str) {
+    const s = str.split(' ')
+    const newStr = []
+    for(let i = 0; i < s.length - 1; i++) {
+        if (s[i] !== s[i + 1]) {
+            newStr.push(s[i])
+        }
+    }
+
+    newStr.push(s[s.length - 1])
+    return newStr.join(' ')
+}
